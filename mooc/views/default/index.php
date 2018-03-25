5cc106b0a041c80d198e36a1913e3c2e35e962be
happyjoblzp <qingzhi.lzp@alibaba-inc.com>
2018-03-25 19:44:09 +0800

    Commit code

---
+++ mooc/views/default/index.php
@@ -0,0 +1,11 @@
+<?php
+use \app\common\services\UrlService;
+?>
+
+<div class="jumbotron body-content">
+    <div class="jumbotron text-center">
+        <img src="<?= UrlService::buildWwwUrl("/images/common/qrcode.jpg"); ?>"/>
+	<!--<img src="/images/common/qrcode.jpg">-->
+        <h3>HM 管理员端功能</h3>
+    </div>
+</div>
